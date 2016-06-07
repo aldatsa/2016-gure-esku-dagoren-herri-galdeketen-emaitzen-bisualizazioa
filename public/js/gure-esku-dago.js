03a8f0b4e1c0e790360927bf6d9a192e05c1804d
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2016-06-07 12:17:59 +0200

    Biztanleria vs partehartzea grafikoa gehitu

--- public/js/gure-esku-dago.js
+++ public/js/gure-esku-dago.js
@@ -131,6 +131,121 @@
         tbody.innerHTML = katea;
     }
 
+    function marraztuBiztanleriaVsPartehartzea(hautatzailea, data) {
+
+        var margin = {top: 20, right: 20, bottom: 30, left: 40},
+            width = 685 - margin.left - margin.right,
+            height = 400 - margin.top - margin.bottom;
+
+        /*
+         * value accessor - returns the value to encode for a given data object.
+         * scale - maps value to a visual display encoding, such as a pixel position.
+         * map function - maps from data value to display value
+         * axis - sets up axis
+         */
+
+        // setup x
+        var xValue = function(d) { return d.biztanleria2014;}, // data -> value
+            xScale = d3.scale.linear().range([0, width]), // value -> display
+            xMap = function(d) { return xScale(xValue(d));}, // data -> display
+            xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+
+        // setup y
+        var yValue = function(d) { return d.partehartzea;}, // data -> value
+            yScale = d3.scale.linear().range([height, 0]), // value -> display
+            yMap = function(d) { return yScale(yValue(d));}, // data -> display
+            yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+        // add the graph canvas to the body of the webpage
+        var svg = d3.select(hautatzailea).append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        // add the tooltip area to the webpage
+        var tooltip = d3.select("body").append("div")
+            .attr("class", "tooltip-biztanleria-vs-partehartzea-grafikoa")
+            .style("opacity", 0);
+
+        var datuak = data.filter(function(element, index, array) {
+            return element.partehartzea;
+        });
+
+        datuak.forEach(function(element, index, array) {
+            datuak[index].biztanleria2014 = parseInt(element.biztanleria2014.replace(/\./, ""), 10);
+            datuak[index].partehartzea = parseFloat(element.partehartzea.replace(",", "."));
+        });
+
+        // don't want dots overlapping axis, so add in buffer to data domain
+        xScale.domain([d3.min(datuak, xValue)-1, d3.max(datuak, xValue)+1]);
+        yScale.domain([d3.min(datuak, yValue)-1, d3.max(datuak, yValue)+1]);
+
+        // x-axis
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+          .append("text")
+            .attr("class", "label")
+            .attr("x", width)
+            .attr("y", 28)
+            .style("text-anchor", "end")
+            .text("Biztanleria 2014");
+
+        // y-axis
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+          .append("text")
+            .attr("class", "label")
+            .attr("transform", "rotate(-90)")
+            .attr("y", -38)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Partehartzea (%)");
+
+        // draw dots
+        svg.selectAll(".dot")
+            .data(datuak)
+          .enter().append("circle")
+            .attr("class", "dot")
+            .attr("r", 3.5)
+            .attr("cx", xMap)
+            .attr("cy", yMap)
+            .style("fill", "#000000")
+            .on("mouseover", function(d) {
+                console.log(d);
+                tooltip.transition()
+                     .duration(200)
+                     .style("opacity", 1);
+                tooltip.html("<div class='izenburua'>" + d.euskarazko_izena + "</div>" +
+                             "<table>" +
+                                "<tr>" +
+                                    "<td>Biztanleria</td>" +
+                                    "<td>" + xValue(d) + "</td>" +
+                                "</tr>" +
+                                "<tr>" +
+                                    "<td>Partehartzea</td>" +
+                                    "<td>%" + yValue(d) + "</td>" +
+                                "</tr>" +
+      	                     "</table>")
+                     .style("top", (d3.event.pageY - 28) + "px");
+
+                // Arrasateren tooltip-a pantailatik ateratzen da bestela.
+                if (d.euskarazko_izena !== "Arrasate") {
+                    tooltip.style("left", (d3.event.pageX + 15) + "px");
+                } else {
+                    tooltip.style("left", (d3.event.pageX - 215) + "px");
+                }
+            })
+            .on("mouseout", function(d) {
+                tooltip.transition()
+                     .duration(500)
+                     .style("opacity", 0);
+            });
+    }
+
     function kalkulatuEhunekoa(balioa, guztira, hamartarrak) {
 
         return (100 * parseInt(balioa, 10) / guztira).toFixed(hamartarrak).replace(/\./g, ',');
@@ -360,6 +475,7 @@
             console.log(herriak.guztira);
 
             bistaratuHerrienDatuenTaula(emaitzak);
+            //marraztuBiztanleriaVsPartehartzea("#biztanleria-vs-partehartzea-grafikoa", emaitzak);
 
             // Udal guztiak.
             svg.selectAll(".unitatea")
@@ -528,6 +644,8 @@
                 .on("mouseout", function(d) {
                     onMouseOut(d);
                 });
+
+            marraztuBiztanleriaVsPartehartzea("#biztanleria-vs-partehartzea-grafikoa", emaitzak);
 
             var biztanleen_grafikoa = c3.generate({
                 bindto: "#biztanleria-grafikoa",
